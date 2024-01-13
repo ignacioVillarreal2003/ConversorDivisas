80b75b63297577b596fac8f5df4434b3abab4bc0
ignacioVillarreal2003 <ignaciovillarreal20031231@gmail.com>
2024-01-13 11:49:46 -0300

    update

---
+++ conversorDivisas/main.py
@@ -0,0 +1,38 @@
+import  requests
+
+API_KEY = 'e85f42056f315e335720edb8'
+URL = 'https://v6.exchangerate-api.com/v6/e85f42056f315e335720edb8/latest/USD'
+
+def get_Tasas():
+    response = requests.get(URL)
+    data = response.json()
+    if (response.status_code != 200):
+        print('Error')
+    else:
+        return data['conversion_rates']
+
+def get_Divisas():
+    print("Valor en USD de cada divisa")
+    tasa = get_Tasas()
+    divisas = ""
+    for e in tasa.keys():
+        divisas += f"{e} - {tasa[e]}\n"
+    print(divisas)
+
+def convertir_moneda(cantidad, monedaInicial, monedaConvertida, monedas):
+    if (monedaInicial != 'USD'):
+        cantidad = cantidad / monedas[monedaInicial]
+    return cantidad * monedas[monedaConvertida]
+
+def main():
+    tasa = get_Tasas()
+    if (tasa):
+        cantidad = float(input('Ingrese el monto a convertir: '))
+        monedaInicial = input('Moneda que quiere convertir: ')
+        monedaConvertida = input('Moneda a la cual quiere convertir: ')
+        resultado = convertir_moneda(cantidad, monedaInicial, monedaConvertida, tasa)
+        print(f'{cantidad} {monedaInicial} = {resultado:.2f} {monedaConvertida}')
+
+get_Divisas()
+if __name__ == '__main__':
+    main()
